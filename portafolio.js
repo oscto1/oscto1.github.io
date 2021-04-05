0ce36b7e08b92870c19c52008e7871cd8d585918
Oscar Castillejo <oscarcastillejo2@gmail.com>
2021-04-05 13:32:16 -0500

    Responsive improvement

--- portafolio.js
+++ portafolio.js
@@ -2,6 +2,13 @@ var slideIndex = [1,1];
 var slideId = ["Slide1","Slide2"];
 showSlides(1, 0);
 showSlides(1, 1);
+var maxHeight = 0;
+var cardssize = document.getElementsByClassName("slideconta");
+var cards = document.getElementById("sampleimg");
+var textsize = document.getElementsByClassName("card-body");
+var textc = document.getElementById("sampletext");
+
+var imagen = document.getElementById("imagenPrinc");
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
@@ -16,4 +23,23 @@ function showSlides(n, no) {
      x[i].style.display = "none";
   }
   x[slideIndex[no]-1].style.display = "block";
+  x[slideIndex[no]-1].style.display = "block";
+}
+
+window.onload = sizecards();
+
+
+function sizecards(){
+
+  if(window.innerWidth < 992){
+    imagen.classList.remove('col-md-8');
+  }
+
+  for(var i=0; i < cardssize.length; i++) {
+    cardssize[i].style.height = cards.offsetHeight+"px";
+  }
+
+  for(var i=0; i < textsize.length; i++) {
+    textsize[i].style.height = (textc.offsetHeight)+"px";
+  }
 }

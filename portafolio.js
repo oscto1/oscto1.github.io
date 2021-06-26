bc549f6386842411fd59df17d5e425cc80e54ddd
Oscar <32046432+oscto1@users.noreply.github.com>
2021-06-25 20:01:00 -0500

    added project slider and performance improvement

--- portafolio.js
+++ portafolio.js
@@ -15,13 +15,13 @@ var language = {
     project2btn: "Try Now",
     project3title: "Messaging app | ReactJS",
     project3text: "A messaging app based on Whatsapp Web. It is built in ReactJS and firebase. You can log in with Google, create rooms, and send or receive text messages from other accounts. I’m still working to improve it.",
-    con: "Lets connect!"
+    con: "Lets connect!",
+    tecs: "Technologies used"
   }
 }
 
 
 if (window.location.hash == "#eng") {
-  console.log("eng");
     $(".aboutme").text(language.eng.about);
     $(".projects").text(language.eng.projects);
     $(".social").text(language.eng.social);
@@ -39,8 +39,26 @@ if (window.location.hash == "#eng") {
     $(".project-3-btn1").text(language.eng.project2btn);
     $(".project-3-btn2").text(language.eng.project1btn);
     $("#con").text(language.eng.con);
+    $(".tecsused").text(language.eng.tecs);
 }
 
+$('.act').click(function (){
+  var t = $(this).attr("id");
+  var id = t.charAt(t.length-1);
+  if($(this).hasClass('desact')){
+    console.log("#dc" + id);
+    $('.f' + id).addClass('fa-angle-down').removeClass('fa-angle-up');
+    $(this).removeClass('desact');
+    $(".dc" + id).animate({height: "90%"}, 150);
+    $(".d" + id).css("overflow-y", "scroll");
+  }else{
+    $('.f' + id).addClass('fa-angle-up').removeClass('fa-angle-down');
+    $(this).addClass('desact');
+    $(".d" + id).scrollTop(0);
+    $(".d" + id).css("overflow-y", "hidden");
+    $(".dc" + id).animate({height: "70px"}, 150);
+  }
+});
 
 $('#englishbtn').click(function (){
   location.hash = "eng";
@@ -52,12 +70,19 @@ $('#espanolbtn').click(function (){
   location.reload();
 });
 
+new Splide( '.splide', {
+	type   : 'loop',
+	padding: {
+		right: '2rem',
+		left : '2rem',
+	},
+} ).mount();
 
-var slideId = ["Slide1","Slide2","Slide3"];
-var slideIndex = [1,1,1];
-showSlides(1, 0);
-showSlides(1, 1);
-showSlides(1, 2);
+// var slideId = ["Slide1","Slide2","Slide3"];
+// var slideIndex = [1,1,1];
+// showSlides(1, 0);
+// showSlides(1, 1);
+// showSlides(1, 2);
 
 var maxHeight = 0;
 var cardssize = document.getElementsByClassName("slideconta");
@@ -67,21 +92,21 @@ var textc = document.getElementById("sampletext");
 
 var imagen = document.getElementById("imagenPrinc");
 
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
-}
-
-function showSlides(n, no) {
-  var i;
-  var x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
-  }
-  x[slideIndex[no]-1].style.display = "block";
-  x[slideIndex[no]-1].style.display = "block";
-}
+// function plusSlides(n, no) {
+//   showSlides(slideIndex[no] += n, no);
+// }
+//
+// function showSlides(n, no) {
+//   var i;
+//   var x = document.getElementsByClassName(slideId[no]);
+//   if (n > x.length) {slideIndex[no] = 1}
+//   if (n < 1) {slideIndex[no] = x.length}
+//   for (i = 0; i < x.length; i++) {
+//      x[i].style.display = "none";
+//   }
+//   x[slideIndex[no]-1].style.display = "block";
+//   x[slideIndex[no]-1].style.display = "block";
+// }
 
 $(document).ready(function() {
 
@@ -94,6 +119,8 @@ $(document).ready(function() {
 });
 
 $(window).on('load', function(){
+  // $('.card-body').height( window.innerWidth / 1.8);
+  $('.card-body').css("background-size", "100.2% 100.5%");
 
   showPage();
 
@@ -101,15 +128,8 @@ $(window).on('load', function(){
     imagen.classList.remove('col-md-8');
   }
 
-  for(var i=0; i < cardssize.length; i++) {
-    cardssize[i].style.height = cards.offsetHeight+"px";
-  }
-
-  for(var i=0; i < textsize.length; i++) {
-    textsize[i].style.height = (textc.offsetHeight)+"px";
-  }
-
 });
+
 
 
 function showPage() {

bf37d32fc1384abb82d7055c3a3490e7dbd05201
Oscar Castillejo <oscarcastillejo2@gmail.com>
2026-04-14 12:27:01 -0500

    Moved standard backdrop-filter after prefixed, and set build target to chrome80 in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import { defineConfig } from "vite";
 export default defineConfig(({ mode }) => ({
   base: mode === "dev"
     ? "/portfolio-dev/"
-    : "/"
+    : "/",
+
+  build: {
+    target: 'chrome80'
+  }
 }));

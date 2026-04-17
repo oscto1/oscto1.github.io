bcff19c706c29a0baafca5156b0678c92f770bd8
Oscar Castillejo <oscarcastillejo2@gmail.com>
2026-04-17 11:25:15 -0500

    Added category to projects

--- js/projects.js
+++ js/projects.js
@@ -6,6 +6,7 @@ const projects = [
             "en": "Forkfest",
             "es": "Forkfest"
         },
+        category: 1,
         description: {
             "en": "C#, Multiplayer application with client-server synchronization and gameplay systems implemented using Unity NGO.",
             "es": "Aplicación multijugador en C# con sincronización cliente-servidor y sistemas de juego implementado usando Unity NGO.",
@@ -26,6 +27,7 @@ const projects = [
             "en": "Math Slopes",
             "es": "Math Slopes"
         },
+        category: 1,
         description: {
             "en": "Browser-based application built with Phaser (JavaScript), implementing game state management, input handling, scoring logic, and dynamic difficulty.",
             "es": "Aplicación para navegador desarrollada con Phaser (JavaScript), que implementa la gestión de estados de juego, manejo de entradas, lógica de puntuación y dificultad dinámica.",
@@ -46,6 +48,7 @@ const projects = [
             "en": "Unity Arcade",
             "es": "Arcade Unity"
         },
+        category: 1,
         description: {
             "en": "C#, Unity project recreating some classic arcade mechanics , implementing input handling, collision detection, scoring systems, and basic UI logic.",
             "es": "Proyecto en C# y Unity que recrea mecánicas de algunos juegos arcade e implementa el manejo de entradas, la detección de colisiones y lógica de interfaz de usuario.",
@@ -66,6 +69,7 @@ const projects = [
             "en": "Sort Algorithms",
             "es": "Algoritmos de ordenamiento"
         },
+        category: 2,
         description: {
             "en": "Interactive visualization tool demonstrating sorting algorithms (Merge, Quick, Heap, Bubble), including step-by-step execution.",
             "es": "Herramienta de visualización interactiva que muestra algoritmos de ordenamiento (Merge, Quick, Heap, Bubble), con ejecución paso a paso.",
@@ -86,6 +90,7 @@ const projects = [
             "en": "Messapp",
             "es": "Messapp"
         },
+        category: 2,
         description: {
             "en": "Chat room application built with React and Firebase, implementing messaging, Google authentication, and cloud-based data synchronization.",
             "es": "Aplicación de salas de chat desarrollada con React y Firebase, que incluye funciones de mensajería, autenticación de Google y sincronización de datos en la nube.",
@@ -162,8 +167,7 @@ async function createProjectCard(project, lang) {
 
     const category = document.createElement("div");
     category.classList.add("card-category");
-    category.innerText = project.year; // + " • " + project.category;
-    
+    category.innerText = project.year + " • " + project.category;
 
     const header = document.createElement("div");
     header.classList.add("card-header");
